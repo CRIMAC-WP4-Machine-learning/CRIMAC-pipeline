adbbc3c7242aa75b6c0963deb855be1d82594915
Nils Olav Handegard <nils.olav.handegard@imr.no>
2022-07-07 17:33:30 +0200

    Test shell script for pallas

--- shellscripts/testS2019847_0511.sh
+++ shellscripts/testS2019847_0511.sh
@@ -1,5 +1,85 @@
-#!/bin/zsh
-echo Hi
-# Sand eel survey
-docker run -it --rm --name unet -v "/mnt/c/DATAscratch/crimac-scratch/2019/S2019847_0511/":/datain -v "/mnt/c/DATAscratch/crimac-scratch/NR_Unet":/model -v "/mnt/c/DATAscratch/crimac-scratch/2019/S2019847_0511/ACOUSTIC/PREDICTIONS/":/dataout --security-opt label=disable --env MODEL="paper_v2_heave_2.pt" --env SURVEY=S2019847_0511 --env ZARRFILE=S2019847_0511_predictions_2.zarr unet:latest
+#!/bin/bash
+# This code runs the testing of the pipeline on the S2019847_0511 data set on Pallas
+# 
+#export DATAIN='/mnt/c/DATAscratch/crimac-scratch/2019/S2019847_0511/'
+#export DATAOUT='/mnt/c/DATAscratch/crimac-scratch/2019/S2019847_0511/'
+export DATAIN='/localscratch_hdd/crimac/2019/S2019847_0511/'
+export DATAOUT='/localscratch_hdd/nilsolav/2019/S2019847_0511/'
 
+export SURVEY='S2019847_0511' # Assume that ${SURVEY}_sv file exit
+
+export PREDICTIONFILE_1=${SURVEY}_labels.zarr
+export REPORTFILE_1=${SURVEY}_report_1.zarr
+export PREDICTIONFILE_2=${SURVEY}_predictions_2.zarr
+export REPORTFILE_2=${SURVEY}_report_2.zarr
+
+export MODEL='/mnt/c/DATAscratch/crimac-scratch/NR_Unet'
+export MODELFILE='paper_v2_heave_2.pt'
+
+# Static variables for report genertor
+export PING_AXIS_INTERVAL_TYPE="distance"  # see http://vocab.ices.dk/?ref=1455
+export PING_AXIS_INTERVAL_ORIGIN="start"  # see http://vocab.ices.dk/?ref=1457
+export PING_AXIS_INTERVAL_UNIT="nmi"  # see http://vocab.ices.dk/?ref=1456
+export PING_AXIS_INTERVAL=0.1
+export CHANNEL_DEPTH_START=0
+export CHANNEL_DEPTH_END=500
+export CHANNEL_THICKNESS=5
+export CHANNEL_TYPE='depth'
+export CLASSTRHRESHOLD=0.8
+export SV_THRESHOLD=-100
+export TYPE="C"  # C = sA, Nautical area scattering coefficient
+export UNIT="m2nmi-2"  # see http://vocab.ices.dk/?ref=1460 |
+export MAIN_FREQ=38000  # The frequency to integrate (could be a list in the future)
+export OUTPUT_TYPE='zarr'
+
+# Sand eel test set Unet prediction
+docker run  -it --rm --name unet \
+       -v "${DATAIN}":/datain \
+       -v "${MODEL}":/model -v "${DATAOUT}/ACOUSTIC/PREDICTIONS/":/dataout \
+       --security-opt label=disable \
+       --env MODEL=$MODELFILE \
+       --env SURVEY=$SURVEY \
+       --env ZARRFILE=$PREDICTIONFILE \
+       unet:latest
+
+# Sand eel test set work file reportgenerator
+docker run -it --rm --name reportgenerator \
+       -v "${DATAIN}ACOUSTIC/GRIDDED":/datain \
+       -v "${DATAIN}ACOUSTIC/GRIDDED":/predin \
+       -v "${DATAOUT}ACOUSTIC/REPORTS"/:/dataout \
+       --security-opt label=disable \
+       --env SURVEY=$SURVEY \
+       --env PREDICTIONFILE=$PREDICTIONFILE_1 \
+       --env REPORTFILE=$REPORTFILE_1 \
+       --env THRESHOLD=$THRESHOLD \
+       --env PING_AXIS_INTERVAL_TYPE=$PING_AXIS_INTERVAL_TYPE \
+       --env PING_AXIS_INTERVAL_ORIGIN=$PING_AXIS_INTERVAL_ORIGIN \
+       --env PING_AXIS_INTERVAL_UNIT=$PING_AXIS_INTERVAL_UNIT \
+       --env PING_AXIS_INTERVAL=$PING_AXIS_INTERVAL \
+       --env CHANNEL_THICKNESS=$CHANNEL_THICKNESS \
+       --env CHANNEL_TYPE=$CHANNEL_TYPE \
+       --env CHANNEL_DEPTH_START=$CHANNEL_DEPTH_START \
+       --env CHANNEL_DEPTH_END=$CHANNEL_DEPTH_END \
+       --env OUTPUT_TYPE=$OUTPUT_TYPE\
+       reportgeneration:latest
+
+# Sand eel test set Unet reportgenerator
+docker run -it --rm --name reportgenerator \
+       -v "${DATAIN}/ACOUSTIC/GRIDDED":/datain \
+       -v "${DATAIN}/ACOUSTIC/PREDICTIONS":/predin \
+       -v "${DATAOUT}/ACOUSTIC/REPORTS"/:/dataout \
+       --security-opt label=disable \
+       --env SURVEY=$SURVEY \
+       --env PREDICTIONFILE=$PREDICTIONFILE_2 \
+       --env REPORTFILE=$REPORTFILE_2 \
+       --env THRESHOLD=$THRESHOLD \
+       --env CLASSTRHRESHOLD=$CLASSTRHRESHOLD \
+       --env PING_AXIS_INTERVAL_TYPE=$PING_AXIS_INTERVAL_TYPE \
+       --env PING_AXIS_INTERVAL_ORIGIN=$PING_AXIS_INTERVAL_ORIGIN \
+       --env PING_AXIS_INTERVAL_UNIT=$PING_AXIS_INTERVAL_UNIT \
+       --env PING_AXIS_INTERVAL=$PING_AXIS_INTERVAL \
+       --env CHANNEL_THICKNESS=$CHANNEL_THICKNESS \
+       --env CHANNEL_TYPE=$CHANNEL_TYPE \
+       --env CHANNEL_DEPTH_START=$CHANNEL_DEPTH_START \
+       --env CHANNEL_DEPTH_END=$CHANNEL_DEPTH_END \
+       reportgeneration:latest
